d5832d665e451200545b99289595df5846a92086
mechanovkirill <mechanovkirill@gmail.com>
2022-10-24 09:52:29 +0600

    Create self code instead PayPal integration.

--- Ecommerce/orders/forms.py
+++ Ecommerce/orders/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Order
+from .models import Order, Payment
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'country', 'city', 'state', 'order_note']
+
+class PaymentForm(forms.Form):
+    class Meta:
+        model = Payment
+        fields = ['payment_id', 'amount_paid']
--- Ecommerce/orders/views.py
+++ Ecommerce/orders/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from carts.models import CartItem
-from .forms import OrderForm
-from .models import Order
+from .forms import OrderForm, PaymentForm
+from .models import Order, Payment
 import datetime
 from decimal import Decimal
+from random import randint
+
 
 # Create your views here.
 
@@ -27,7 +29,7 @@ def place_order_view(request, total=0, quantity=0):
         form = OrderForm(request.POST)
         if form.is_valid():
             # store all billing info inside Order DB
-            order_db_data = Order() #TODO: authomatic get data from account
+            order_db_data = Order()  # TODO: authomatic get data from account
             order_db_data.user = current_user
             order_db_data.first_name = form.cleaned_data['first_name']
             order_db_data.last_name = form.cleaned_data['last_name']
@@ -40,14 +42,14 @@ def place_order_view(request, total=0, quantity=0):
             order_db_data.order_note = form.cleaned_data['order_note']
             order_db_data.order_total = Decimal(total_plus_tax)
             order_db_data.tax = Decimal(tax)
-            order_db_data.ip = request.META.get('REMOTE_ADDR') # get user's ip
+            order_db_data.ip = request.META.get('REMOTE_ADDR')  # get user's ip
             # https://docs.djangoproject.com/en/4.1/ref/request-response/
             order_db_data.save()
 
             year = int(datetime.date.today().strftime('%Y'))
             date = int(datetime.date.today().strftime('%d'))
             month = int(datetime.date.today().strftime('%m'))
-            d = datetime.date(year,month,date)
+            d = datetime.date(year, month, date)
             current_date = d.strftime('%Y%m%d')
             order_number = current_date + str(order_db_data.id)
             order_db_data.order_number = order_number
@@ -68,4 +70,39 @@ def place_order_view(request, total=0, quantity=0):
 
 
 def payments_view(request):
+    ''' Instead of Paypal, here is a gag code (a citizen of Russia cannot register Paypal)
+    '''
+    if request.method == 'POST':
+        order = Order.objects.get(user=request.user, is_ordered=False, order_number=request.POST['payment_id'])
+        try:
+            payment_db_data = Payment()
+            payment_db_data.user = request.user
+            payment_db_data.payment_id = order.order_number + datetime.datetime.now().strftime('%H%M')
+            print(type(order.order_number))
+            payment_db_data.amount_paid = order.order_total
+            payment_db_data.payment_method = 'PayPal'
+            payment_db_data.status = 'COMPLETED'
+            payment_db_data.save()
+
+            payment = Payment.objects.get(user=request.user, payment_id=payment_db_data.payment_id)
+
+            order.payment = payment
+            order.is_ordered = True
+            order.save()
+            # end of a gag code
+            return redirect('orders:payments-view')
+        except:
+            pass
+        # move the cart items to order product table
+
+        # reduce the quantity of sold products
+
+        # clear cart
+
+        # Send order received email to customer
+
+        # send order number and transaction id back to js
+    else:
+        return render(request, 'orders/payments.html')
+
     return render(request, 'orders/payments.html')
